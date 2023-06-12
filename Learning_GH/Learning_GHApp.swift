26858dc24bbb04cb5c1ffb7195a3409e45377037
Aadi Ghai <aaditheghai@gmail.com>
2023-06-11 23:49:00 -0500

    Initial Commit

---
+++ Learning_GH/Learning_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GHApp.swift
+//  Learning_GH
+//
+//  Created by Aadi Ghai on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
